2513e2d0c2a5b61a0554708e08d56a7374645ac3
🦊Tomoya Hirano <noppelabs@gmail.com>
2018-02-27 00:38:58 +0900

    add shared scheme

---
+++ Carthage/CodableMapper/CodableMapper/CodableMapper.h
@@ -0,0 +1,19 @@
+//
+//  CodableMapper.h
+//  CodableMapper
+//
+//  Created by Tomoya Hirano on 2018/02/27.
+//  Copyright © 2018年 Tomoya Hirano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CodableMapper.
+FOUNDATION_EXPORT double CodableMapperVersionNumber;
+
+//! Project version string for CodableMapper.
+FOUNDATION_EXPORT const unsigned char CodableMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodableMapper/PublicHeader.h>
+
+
